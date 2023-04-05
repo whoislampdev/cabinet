ef73fe3cc8b596745e8018146a17203df657e61c
omzo <omzo@example.com>
2023-04-05 15:01:25 +0100

    cabinet

--- cabinet/app/Http/Controllers/TicketController.php
+++ cabinet/app/Http/Controllers/TicketController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ticket;
+use App\Models\TypeTicket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
--- cabinet/resources/views/pharmacie/index.blade.php
+++ cabinet/resources/views/pharmacie/index.blade.php
@@ -1,5 +1,89 @@
 @extends('pharmacie.base')
 
 @section('main')
-    <h1 style="text-align: center; margin-top:80px;">bienvenue dans la Pharmacie</h1>
+<div class="row">
+                    <div class="col-md-6 col-xl-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="mb-4">
+                                    <span class="badge badge-soft-primary float-right">Daily</span>
+                                    <h5 class="card-title mb-0">Medicaments</h5>
+                                </div>
+                                <div class="row d-flex align-items-center mb-4">
+                                    <div class="col-8">
+                                        <h2 class="d-flex align-items-center mb-0">
+                                            125
+                                        </h2>
+                                    </div>
+                                    <!-- <div class="col-4 text-right">
+                                        <span class="text-muted">12.5% <i class="mdi mdi-arrow-up text-success"></i></span>
+                                    </div> -->
+                                </div>
+
+                                <div class="progress shadow-sm" style="height: 5px;">
+                                    <div class="progress-bar bg-success" role="progressbar" style="width: 57%;">
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end card body-->
+                        </div><!-- end card-->
+                    </div> <!-- end col-->
+
+                    <div class="col-md-6 col-xl-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="mb-4">
+                                    <span class="badge badge-soft-primary float-right">Per Week</span>
+                                    <h5 class="card-title mb-0">Categories</h5>
+                                </div>
+                                <div class="row d-flex align-items-center mb-4">
+                                    <div class="col-8">
+                                        <h2 class="d-flex align-items-center mb-0">
+                                            269
+                                        </h2>
+                                    </div>
+                                    <!-- <div class="col-4 text-right">
+                                        <span class="text-muted">18.7% <i class="mdi mdi-arrow-down text-danger"></i></span>
+                                    </div> -->
+                                </div>
+
+                                <div class="progress shadow-sm" style="height: 5px;">
+                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 57%;">
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end card body-->
+                        </div><!-- end card-->
+                    </div> <!-- end col-->
+
+                    <div class="col-md-6 col-xl-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="mb-4">
+                                    <span class="badge badge-soft-primary float-right">Per Month</span>
+                                    <h5 class="card-title mb-0">Medicament Vendu</h5>
+                                </div>
+                                <div class="row d-flex align-items-center mb-4">
+                                    <div class="col-8">
+                                        <h2 class="d-flex align-items-center mb-0">
+                                            369
+                                        </h2>
+                                    </div>
+                                    <!-- <div class="col-4 text-right">
+                                        <span class="text-muted">57% <i class="mdi mdi-arrow-up text-success"></i></span>
+                                    </div> -->
+                                </div>
+
+                                <div class="progress shadow-sm" style="height: 5px;">
+                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 57%;">
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end card body-->
+                        </div>
+                        <!--end card-->
+                    </div> <!-- end col-->
+
+<!-- end col-->
+                </div>
 @endsection
